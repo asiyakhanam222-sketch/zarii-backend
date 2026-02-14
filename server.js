50107afe5e724f9317a9285887e4faf7a422b220
Asiya <asiyakhanam222@gmail.com>
2026-02-14 11:19:51 +0530

    Fixed checkout link

--- server.js
+++ server.js
@@ -142,6 +142,7 @@ app.get("/api/orders", async (req, res) => {
 });
 
 /* ===== START SERVER ===== */
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+   console.log(`Server running on port ${PORT}`);
 });
